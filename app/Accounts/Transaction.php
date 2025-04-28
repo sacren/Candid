2297174793aac2a493450cc3372fd0c9c0c78585
Jean Sacren <sakiwit@gmail.com>
2025-04-27 19:55:03 -0600

    Add Transaction class
    Use make:class to build Transaction class scaffolding.
    
    $ sail artisan make:class -- Accounts/Transaction
    
       INFO  Class [app/Accounts/Transaction.php] created successfully.
    
    Use make:view to generate Transaction blade view scaffolding and build
    with the minimal view.
    
    $ sail artisan make:view -- transaction/index
    
       INFO  View [resources/views/transaction/index.blade.php] created successfully.
    
    Use make:controller to generate Transaction controller scaffolding.
    
    $ sail artisan make:controller --resource --model -- TransactionController
    
       INFO  Controller [app/Http/Controllers/TransactionController.php] created successfully.
    
    Add the route for transactions.

---
+++ app/Accounts/Transaction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Accounts;
+
+class Transaction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Transaction Details</title>
+</head>
+<body>
+    <h1>Transaction Details</h1>
+    @php
+        var_dump($transaction);
+    @endphp
+</body>
+</html>
