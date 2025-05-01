a8bb3048a1456956080697e7f0d003b0b4262e28
Jean Sacren <sakiwit@gmail.com>
2025-05-01 15:39:33 -0600

    Fix type inconsistency
    The $amount property is declared as float but is assigned a string value
    through number_format function call.  This violates the type hinting and
    could lead to runtime errors or unexpected behavior when interacting
    with $amount elsewhere in the code.  For example, if we try to perform
    arithmetic operations on $amount, we will encounter issues because it is
    no longer a float but a string.
    
    The model, in this case ProcessTransaction, should focus on storing and
    managing the raw data.  The view is responsible for presenting the data
    in a user-friendly format.  Thus, move number_format call to the view.
    
    By keeping the raw float value in the model, we retain the ability to
    use it for calculations or other operations without being constrained by
    a pre-formatted string.  In the view, we can decide how to format the
    value based on the context for currency, decimal places, locale-specific
    formatting, or whatever.

--- app/Accounts/ProcessTransaction.php
+++ app/Accounts/ProcessTransaction.php
@@ -5,17 +5,9 @@ namespace App\Accounts;
 class ProcessTransaction
 {
     /**
-     * Public properties for amount and description.
-     */
-    public float $amount;
-    public string $description;
-
-    /**
      * Create a helper class instance.
      */
-    public function __construct(float $amount, string $description)
+    public function __construct(public float $amount, public string $description)
     {
-        $this->amount = number_format($amount, 2);
-        $this->description = $description;
     }
 }
--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -7,7 +7,7 @@
     <h1>Transaction Details</h1>
     @foreach ($data as $item)
         <p><strong>Description:</strong> {{ $item->description }}</p>
-        <p><strong>Final Amount:</strong> {{ $item->amount }} </p>
+        <p><strong>Final Amount:</strong> {{ number_format($item->amount, 2) }}</p>
     @endforeach
 </body>
 </html>
