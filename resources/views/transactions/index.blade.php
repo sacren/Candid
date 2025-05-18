f7cef31a6a093394927cd3b09b5f05769ac64cda
Jean Sacren <sakiwit@gmail.com>
2025-05-17 23:43:22 -0600

    Build new index view for transactions
    Create the index view scaffolding using make:view.
    
    $ sail artisan make:view -- transactions/index
    
       INFO  View [resources/views/transactions/index.blade.php] created successfully.
    
    Build the index view for transactions based on Livewire layout.  Use
    part of the content of the existing transaction/index view to further
    build the view for transactions.

---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app title="Transaction Home">
+    <h1>Transaction Details</h1>
+
+    @foreach ($data as $item)
+        <p><strong>Description:</strong> {{ $item->description }}</p>
+        <p><strong>Final Amount:</strong> {{ number_format($item->amount, 2) }}</p>
+    @endforeach
+</x-layouts.app>
