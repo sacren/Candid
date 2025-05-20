4fd8ba45ece9afd2fa0d7ff0bcc9544e41ec6042
Jean Sacren <sakiwit@gmail.com>
2025-05-20 15:26:03 -0600

    Use nullsafe operator
    The nullsafe operator (?->) was introduced in PHP 8.0 to make it easier
    and cleaner to access properties or call methods on objects that may be
    null.
    
    It works like the regular object operator (->), but if the left-hand
    side is null, it short-circuits and returns null instead of throwing an
    error.
    
    Without the nullsafe operator, accessing deeply nested properties can be
    risky and it will throw an error if any part of the chain is null (e.g.,
    $item->customer is null).
    
    In our example, if $item->customer or $item->customer->paymentProfile is
    null, the whole expression evaluates to null.

--- app/Accounts/ProcessTransaction.php
+++ app/Accounts/ProcessTransaction.php
@@ -5,6 +5,11 @@ namespace App\Accounts;
 class ProcessTransaction
 {
     /**
+     * The customer associated with the transaction.
+     */
+    public ?Customer $customer = null;
+
+    /**
      * Create a helper class instance.
      */
     public function __construct(public float $amount, public string $description)
