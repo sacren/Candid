98a629c97022eddb4b589f92dc9e34065f924d35
Jean Sacren <sakiwit@gmail.com>
2025-05-24 20:13:24 -0600

    Add status constants in Transaction class
    Build a list of status constants in Transaction class using the const
    keyword.  Declare the constants as public since we intend to reference
    them from outside the class.
    
    Use fully qualified class name and the scope resolution operator (::) to
    access the class constants in the Blade view.  This shows why we declare
    the constants public.

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -5,5 +5,6 @@
         <p><strong>ID:</strong> {{ $item->getCustomer()?->getPaymentProfile()?->id ?? 'N/A' }}</p>
         <p><strong>Description:</strong> {{ $item->description }}</p>
         <p><strong>Final Amount:</strong> {{ number_format($item->amount, 2) }}</p>
+        <p><strong>Status:</strong> {{ \App\Accounts\Transaction::STATUS_PAID }}</p>
     @endforeach
 </x-layouts.app>
